c25a38c8752c25aa83f66f7de97a94c6e9cfe766
Yahir <202300155@upqroo.edu.mx>
2026-06-05 14:04:27 -0500

    Cambios corregidos, pantallas, destellos y teclado numerico

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -56,23 +56,19 @@ class LoginScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-
                 const SizedBox(height: 40),
 
                 // input gmail
                 const InputWidget(label: 'Correo Electrónico'),
-
                 const SizedBox(height: 20),
 
                 // input contraseña
                 const InputWidget(label: 'Contraseña', obscureText: true),
-
                 const SizedBox(height: 40),
 
                 // boton
                 SizedBox(
                   width: 150,
-
                   child: ButtonWidget(texto: 'Aceptar', onPressed: () {}),
                 ),
 
