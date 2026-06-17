b3c2e9718a220ba1ce0485ec99deee674095eec3
Yahir <202300155@upqroo.edu.mx>
2026-06-17 14:41:59 -0500

    Funcionamiento del login y register con la base de datos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
 import 'package:chocomil_movies_app_bv/theme/app_theme.dart';
 import 'package:chocomil_movies_app_bv/config/router/app_router.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
